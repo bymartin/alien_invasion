8506005f0f7f3020db0a2665e7bb87e24619ee62
Barry Martin <barrymartintx@gmail.com>
2021-03-16 15:54:16 -0500

    Template for the game

---
+++ alien_invasion.py
@@ -0,0 +1,48 @@
+import sys
+import os
+
+import pygame
+
+# using Ubuntu with WSL 1
+# this error happened from pygame.init()
+# ALSA lib pcm.c:2642:(snd_pcm_open_noupdate) Unknown PCM default
+# workaround below to use a different sound driver
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch the keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+        
+
+    
