b1efcb401c637b6216de580e72186dcd70446521
Barry Martin <barrymartintx@gmail.com>
2021-03-18 10:47:24 -0500

    Refactor run_game with 2 helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,12 +31,17 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch the keyboard and mouse events.
+            self._check_events()
+            self._update_screen()         
+
+    def _check_events(self):
+        # Watch the keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Redraw the screen during each pass through the loop
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
             # Draw the ship
             self.ship.blitme()
